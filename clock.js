8fffe48d5a4996ea638f1af7026ff72d9ed45a85
Christof Jans <christofjans@gmail.com>
2020-12-15 07:50:50 -0500

    added clock

---
+++ clock.js
@@ -0,0 +1,38 @@
+export function runclock(canvasId) {
+    var canvas = document.getElementById(canvasId);
+    var ctx = canvas.getContext('2d');
+    if (ctx === null) {
+        console.error('unable to retrieve context');
+    }
+    else {
+        setInterval(function () {
+            drawClock(ctx);
+        }, 200);
+    }
+}
+function drawClock(ctx) {
+    ctx.resetTransform();
+    ctx.clearRect(0, 0, 256, 256);
+    /*let hours = 9;
+    let minutes = 30;
+    let seconds = 0;*/
+    var now = new Date();
+    var seconds = now.getSeconds();
+    var minutes = now.getMinutes();
+    var hours = now.getHours();
+    minutes += seconds / 60;
+    if (hours >= 12)
+        hours -= 12;
+    hours += minutes / 60;
+    hours *= 5;
+    drawArm(ctx, seconds, 2, 120, '#ff0000');
+    drawArm(ctx, minutes, 5, 100, '#000000');
+    drawArm(ctx, hours, 10, 80, '#000000');
+}
+function drawArm(ctx, seconds, width, height, fill) {
+    ctx.fillStyle = fill;
+    ctx.resetTransform();
+    ctx.translate(128, 128);
+    ctx.rotate(seconds * 2 * Math.PI / 60);
+    ctx.fillRect(-width / 2, -height, width / 2, height);
+}
